fde4ec84fe7354a6daebf5267f7537525bd364fa
Benek <beniamin.bartkowiak@amway.com>
2015-08-25 15:43:45 +0200

    ajaxfying the comments

--- tutorial_r/Scripts/Tutorial.jsx
+++ tutorial_r/Scripts/Tutorial.jsx
@@ -1,11 +1,4 @@
-﻿var data = [
-  { Author: "Daniel Lo Nigro", Text: "Hello ReactJS.NET World!" },
-  { Author: "Pete Hunt", Text: "This is one comment" },
-  { Author: "Jordan Walke", Text: "This is *another* comment" }
-];
-
-
-var Comment = React.createClass({
+﻿var Comment = React.createClass({
   render: function() {
     return (
 	  <div className="comment">
@@ -46,31 +39,35 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', this.props.url, true);
+    xhr.onload = function() {
+      var data = JSON.parse(xhr.responseText);
+      this.setState({ data: data });
+    }.bind(this);
+    xhr.send();
+  },
   getInitialState: function() {
     return {data: []};
   },
-  componentWillmount: function() {
-    var xhr = new XMLHttpRequest();
-	xhr.open('get',this.props.url,true);
-	xhr.onload = function() {
-	  var data = JSON.parse(xhr.responseText);
-	  this.setState({ data: data });
-	}.bind(this);
-	xhr.send();
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.state.data}/>
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
   }
 });
 
-
 React.render(
-  <CommentBox data={data} />,
+  <CommentBox url="/comments" pollInterval={2000} />,
   document.getElementById('content')
 );
+
